00bafb9bfd88657839d137a0431e03ac853fda9f
Devon Gardner <devon@goosur.com>
2026-01-04 15:47:01 -0500

    feat: reuse new plant body when editing existing plant

--- app/src/main/java/com/example/garden/ui/screen/PlantEditScreen.kt
+++ app/src/main/java/com/example/garden/ui/screen/PlantEditScreen.kt
@@ -1,10 +1,16 @@
 package com.example.garden.ui.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.garden.ui.viewmodel.PlantEditViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun PlantEditScreen(
@@ -12,5 +18,21 @@ fun PlantEditScreen(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val plantUiState by viewModel.plantUiState.collectAsState()
+    Scaffold(modifier.fillMaxSize()) { innerPadding ->
+        PlantEntryBody(
+            plantUiState = plantUiState,
+            onClick = {
+                coroutineScope.launch {
+                    viewModel.updatePlant()
+                    onSaveClick()
+                }
+            },
+            updateUiState = viewModel::updateUiState,
+            modifier = modifier
+                .padding(innerPadding)
+                .safeContentPadding()
+        )
+    }
 }
