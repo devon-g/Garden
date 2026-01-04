fb0151e57b60b98bb405bf991a7e3945d49258fd
Devon Gardner <devon@goosur.com>
2026-01-04 12:23:22 -0500

    feat: Add top app bar to main screen

--- app/src/main/java/com/example/garden/ui/screen/GardenScreen.kt
+++ app/src/main/java/com/example/garden/ui/screen/GardenScreen.kt
@@ -1,27 +1,32 @@
 package com.example.garden.ui.screen
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.garden.R
 import com.example.garden.ui.viewmodel.GardenViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GardenScreen(
     onAddEntryClick: () -> Unit,
     viewModel: GardenViewModel,
     modifier: Modifier = Modifier,
 ) {
-    val gardenUiState = viewModel.gardenUiState
+    val gardenUiState by viewModel.gardenUiState.collectAsState()
 
     Scaffold(
         modifier = modifier,
@@ -34,10 +39,20 @@ fun GardenScreen(
                     contentDescription = stringResource(R.string.add_new_plant_entry)
                 )
             }
+        },
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.garden_screen_title))
+                },
+            )
         }
     ) { innerPadding ->
-        LazyColumn(modifier = Modifier.padding(innerPadding)) {
-            items(gardenUiState.value.plants) {
+        LazyColumn(
+            contentPadding = innerPadding,
+            modifier = Modifier.safeContentPadding()
+        ) {
+            items(gardenUiState.plants) {
                 // TODO: Make a clickable card that navigates to a screen with more details
                 Text(text = it.species)
             }
