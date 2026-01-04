0c122f51a42c74df77328353f3f3c3a9de612f19
Devon Gardner <devon@goosur.com>
2026-01-04 15:45:37 -0500

    feat: enable scrolling when content clips screen

--- app/src/main/java/com/example/garden/ui/screen/PlantEntryScreen.kt
+++ app/src/main/java/com/example/garden/ui/screen/PlantEntryScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -76,7 +78,9 @@ fun PlantEntryBody(
 ) {
     Column(
         verticalArrangement = Arrangement.spacedBy(20.dp),
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(state = rememberScrollState())
     ) {
         PlantDetailForm(
             plantDetails = plantUiState.plantDetails,
