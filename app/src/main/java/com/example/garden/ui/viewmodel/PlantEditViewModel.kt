d78c9d94c409248b8df1ccd8850228a1cbf3b296
Devon Gardner <devon@goosur.com>
2026-01-04 15:46:26 -0500

    feat: initialize plant edit view model with injected plantId from navigation

--- app/src/main/java/com/example/garden/ui/viewmodel/PlantEditViewModel.kt
+++ app/src/main/java/com/example/garden/ui/viewmodel/PlantEditViewModel.kt
@@ -1,18 +1,40 @@
 package com.example.garden.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.garden.data.GardenRepository
+import com.example.garden.data.Plant
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class PlantEditViewModel(private val gardenRepository: GardenRepository) : ViewModel() {
+class PlantEditViewModel(
+    private val plantId: Int,
+    private val gardenRepository: GardenRepository
+) : ViewModel() {
     /**
      * Current UI State
      */
     private val _plantUiState = MutableStateFlow(PlantUiState())
     val plantUiState: StateFlow<PlantUiState> = _plantUiState.asStateFlow()
+
+    /**
+     * Get plantId information from repository using a coroutine to initialize state
+     */
+    init {
+        viewModelScope.launch {
+            _plantUiState.update {
+                gardenRepository.getPlantStream(plantId)
+                    .filterNotNull()
+                    .first()
+                    .toPlantUiState(true)
+            }
+        }
+    }
 
     fun updateUiState(plantDetails: PlantDetails) {
         _plantUiState.update { uiState ->
@@ -29,3 +51,18 @@ class PlantEditViewModel(private val gardenRepository: GardenRepository) : ViewM
         }
     }
 }
+
+fun Plant.toPlantUiState(isEntryValid: Boolean = false): PlantUiState = PlantUiState(
+    plantDetails = this.toPlantDetails(),
+    isEntryValid = isEntryValid
+)
+
+fun Plant.toPlantDetails(): PlantDetails = PlantDetails(
+    id = id,
+    species = species,
+    //synonyms = synonyms,
+    lightLevel = lightLevel,
+    needsDirectLight = needsDirectLight,
+    isNative = isNative,
+    notes = notes
+)
