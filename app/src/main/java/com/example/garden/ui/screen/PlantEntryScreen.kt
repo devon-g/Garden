787122a28f3bb104e3b199014a5a7ce3a5eeeebb
Devon Gardner <devon@goosur.com>
2026-01-04 01:51:35 -0500

    refactor: renamed view package to screen

---
+++ app/src/main/java/com/example/garden/ui/screen/PlantEntryScreen.kt
@@ -0,0 +1,18 @@
+package com.example.garden.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.garden.ui.theme.GardenTheme
+
+@Composable
+fun PlantEntryScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PlantEntryScreenPreview() {
+    GardenTheme {
+        PlantEntryScreen()
+    }
+}
